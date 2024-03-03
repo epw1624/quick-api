1d8f6a0ea1760ba65a148b1902d208220114e446
Emily Wood <emily.wood1624@gmail.com>
2024-03-03 14:50:36 -0800

    add type specifier

--- quick-api/src/api_handler.rs
+++ quick-api/src/api_handler.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use serde_json;
 
 pub async fn get(client: reqwest::Client, request_url: String) -> Result<serde_json::Value, Box<dyn Error>> {
-    let response = client.get(&request_url).send().await?.text().await?;
+    let response: String = client.get(&request_url).send().await?.text().await?;
     let v: serde_json::Value = serde_json::from_str(&response).unwrap();
     println!("{:?}", v);
     
