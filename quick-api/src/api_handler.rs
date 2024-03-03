6bb26b00b2f33dfe010722aecde45dee0c6a1f7d
Emily Wood <emily.wood1624@gmail.com>
2024-03-03 10:35:39 -0800

    add serde_json for json parsing

--- quick-api/src/api_handler.rs
+++ quick-api/src/api_handler.rs
@@ -1,10 +1,11 @@
 use std::error::Error;
+use serde_json;
 
 pub async fn get(request_url: String) -> Result<(), Box<dyn Error>> {
     println!("{}", request_url);
-    let response = reqwest::get(&request_url).await?;
-
-    println!("{:?}", response);
+    let response = reqwest::get(&request_url).await?.text().await?;
+    let v: serde_json::Value = serde_json::from_str(&response).unwrap();
+    println!("{:?}", v);
     
     Ok(())
 }
