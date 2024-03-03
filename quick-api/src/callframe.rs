af9bc9c901b404ca74dae0529e2615693e75ba00
Emily Wood <emily.wood1624@gmail.com>
2024-03-03 14:50:57 -0800

    make_request method for Callframe objects

--- quick-api/src/callframe.rs
+++ quick-api/src/callframe.rs
@@ -1,21 +1,20 @@
 use std::collections::HashMap;
 use serde_json;
 
-enum HTTP_Method {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    PATCH,
-}
-
 struct Callframe {
     url: string,
-    method: HTTP_Method,
+    method: reqwest::Method,
     headers: HashMap<String, String>,
     result: serde_json::Value,
 }
 
 impl Callframe {
+    fn make_request(&self) -> Result<serde_json::Value, Box<dyn Error>> {
+        let client = reqwest::Client::new();
+        let response = client.request(self.method, self.url).send().await?.text().await?;
+        let v = serde_json::from_str(&response).unwrap();
+        println!("{:?}", v);
     
+        Ok(v)
+    }
 }
