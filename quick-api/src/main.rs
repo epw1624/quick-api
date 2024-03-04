f3871e53cd2c1ed6fb2b8c9f96cd4ee6eab4008c
Emily Wood <emily.wood1624@gmail.com>
2024-03-03 21:20:29 -0800

    move command line interaction to build_callframe function

--- quick-api/src/main.rs
+++ quick-api/src/main.rs
@@ -1,5 +1,3 @@
-use std::io;
-use std::collections::HashMap;
 use futures::executor::block_on;
 
 mod api_handler;
@@ -10,19 +8,7 @@ async fn main() {
     println!("quick-api: A command line interface for prototyping API calls");
 
     loop {
-        println!("Type or paste a URL to make an API call");
-
-        let mut url = String::new();
-        io::stdin()
-            .read_line(&mut url)
-            .expect("Failed to read line");
-
-        let mut callframe = callframe::Callframe {
-            url,
-            method: reqwest::Method::GET,
-            headers: HashMap::new(),
-            response: None, 
-        };
+        let mut callframe: callframe::Callframe = callframe::Callframe::build_callframe();
 
         let future = callframe.make_request();
         let _ = block_on(future);
