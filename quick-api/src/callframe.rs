8fabe7e91d3370d7fbc357a6f471d4a950d32808
Emily Wood <emily.wood1624@gmail.com>
2024-03-29 15:48:01 -0700

    add clap crate for nicer cli flow

--- quick-api/src/callframe.rs
+++ quick-api/src/callframe.rs
@@ -13,48 +13,12 @@ pub struct Callframe {
 }
 
 impl Callframe {
-    // temporarily, an array to index for selecting method, make nicer with a cli crate like clap later
-    const METHODS: [reqwest::Method; 4] = [reqwest::Method::GET, reqwest::Method::POST, reqwest::Method::PUT, reqwest::Method::DELETE]; 
-    pub fn build_callframe() -> Callframe {
-        println!("Type or paste the request URL");
-        let mut url = String::new();
-        io::stdin()
-            .read_line(&mut url)
-            .expect("Failed to read line");
-
-        println!("Enter a number to select an HTTP request method\n
-        1) GET\n
-        2) POST\n
-        3) PUT\n
-        4) DELETE");
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-        let parsed_input: Result<i32, _> = input.trim().parse();
-        match parsed_input {
-            Ok(mut number) => {
-                number -= 1;
-                Callframe {
-                    url,
-                    method: Self::METHODS[number as usize].clone(),
-                    headers: HashMap::new(),
-                    response: None
-                }
-            }
-            Err(err) => {
-                eprintln!("Error: {}", err);
-                std::process::exit(1);
-            }
-        }
-    }
     
     pub async fn make_request(&mut self) -> Result<serde_json::Value, Box<dyn Error>> {
         let client = reqwest::Client::new();
         let response = client.request(self.method.clone(), &self.url).send().await?.text().await?;
         let v: serde_json::Value = serde_json::from_str(&response).unwrap();
         self.response = Some(v.clone());
-        println!("{:?}", self.response);
 
         Ok(v)
     }
--- quick-api/src/main.rs
+++ quick-api/src/main.rs
@@ -1,19 +1,47 @@
+use callframe::Callframe;
+use clap::{command, Arg};
+use std::collections::HashMap;
 use futures::executor::block_on;
 
-mod api_handler;
 mod callframe;
 
 #[tokio::main]
 async fn main() {
-    println!("quick-api: A command line interface for prototyping API calls");
 
-    // loop {
-        let mut callframe: callframe::Callframe = callframe::Callframe::build_callframe();
+    let match_result = command!()
+    .about("quick-api: A command line interface for prototyping API calls")
+    .arg(Arg::new("url")
+        .long("url")
+        .help("URL of the API call")
+        .required(true)
+    ).arg(Arg::new("method")
+        .long("method")
+        .help("HTTP method")
+        .value_parser(["GET", "POST", "PUT", "DELETE"])
+        .required(true)
+    ).get_matches();
 
-        let future = callframe.make_request();
-        let _ = block_on(future);
+    let url: String = match_result.get_one::<String>("url").unwrap().to_string();
+    let method_string: String = match_result.get_one::<String>("method").unwrap().to_string();
 
-        // write the callframe to a file
-        let _ = callframe::Callframe::save_callframe(&callframe, "test_output.json");
-    // }
+    let method = match method_string.as_str() {
+        "GET" => reqwest::Method::GET,
+        "POST" => reqwest::Method::POST,
+        "PUT" => reqwest::Method::PUT,
+        "DELETE" => reqwest::Method::DELETE,
+        _ => reqwest::Method::GET // this should never happen because of value_parser() but I needed to include a default case
+    };
+
+    let mut callframe = Callframe {
+        url,
+        method,
+        headers: HashMap::new(),
+        response: None
+    };
+
+    let future = callframe.make_request();
+    let _ = block_on(future);
+
+    let _ = Callframe::save_callframe(&callframe, "test_output.json");
+
 }
